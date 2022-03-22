1074423f2596e4804ca64449bd47f659f50a7ad9
Sjackson199703 <55985199+Sjackson199703@users.noreply.github.com>
2022-03-22 13:01:30 +0000

    Add files via upload
    Website

---
+++ js.js
@@ -0,0 +1,38 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function currentSlides(n){
+    showSlides(slideIndex = n)
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  var dots2 = document.getElementsByClassName("dot2");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  for (i = 0; i < dots2.length; i++) {
+    dots2[i].className = dots2[i].className.replace(" active", "");
+}
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  dots2[slideIndex-1].className += " active";
+}
